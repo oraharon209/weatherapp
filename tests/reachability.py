bfa11aa60d7676f6410e70785fb459d73a202473
Or Aharon <or.aharon209@gmail.com>
2024-05-28 13:38:36 +0300

    added logs

--- tests/reachability.py
+++ tests/reachability.py
@@ -3,17 +3,25 @@ import time
 
 def test_site_up():
     print("Running test_site_up...")
-    output = subprocess.check_output("curl http://127.0.0.1:9090 --max-time 3", shell=True)
-    print("Output:", output)
-    assert "Failed" not in str(output)
+    try:
+        output = subprocess.check_output("curl http://127.0.0.1:9090 --max-time 3", shell=True)
+        print("Output:", output)
+        assert "Failed" not in str(output)
+    except subprocess.CalledProcessError as e:
+        print(f"Failed to connect: {e}")
+        raise
 
 def test_connection():
     print("Running test_connection...")
-    output = subprocess.check_output("curl --include http://127.0.0.1:9090", shell=True)
-    print("Output:", output)
-    status_code = int(str(output).split(" ")[1])
-    print("Status Code:", status_code)
-    assert status_code in range(200, 400)
+    try:
+        output = subprocess.check_output("curl --include http://127.0.0.1:9090", shell=True)
+        print("Output:", output)
+        status_code = int(str(output).split(" ")[1])
+        print("Status Code:", status_code)
+        assert status_code in range(200, 400)
+    except subprocess.CalledProcessError as e:
+        print(f"Connection error: {e}")
+        raise
 
 if __name__ == '__main__':
     time.sleep(5)  # Add a short sleep to ensure the service has time to start
