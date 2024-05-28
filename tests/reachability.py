6518d7a498eefee5e67c84da83dafa14d90a3a7a
Or Aharon <or.aharon209@gmail.com>
2024-05-28 14:18:53 +0300

    test

--- tests/reachability.py
+++ tests/reachability.py
@@ -3,7 +3,7 @@ import subprocess
 def test_site_up():
     print("Running test_site_up...")
     try:
-        output = subprocess.check_output("curl -s http://weatherapp_container:9090 --max-time 3", shell=True)
+        output = subprocess.check_output("curl -s http://127.0.0.1:9090 --max-time 3", shell=True)
         print("Output:", output)
         assert b"Failed" not in output
     except subprocess.CalledProcessError as e:
@@ -13,7 +13,7 @@ def test_site_up():
 def test_connection():
     print("Running test_connection...")
     try:
-        output = subprocess.check_output("curl --include http://weatherapp_container:9090", shell=True)
+        output = subprocess.check_output("curl --include http://127.0.0.1:9090", shell=True)
         print("Output:", output)
         status_code = int(output.split()[1])
         print("Status Code:", status_code)
