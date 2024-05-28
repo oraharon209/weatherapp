25716036f7383b9e5b328987a056683bc47a76e7
Or Aharon <or.aharon209@gmail.com>
2024-05-28 14:09:47 +0300

    new test try

--- tests/reachability.py
+++ tests/reachability.py
@@ -1,15 +1,11 @@
-import os
 import subprocess
-import time
-
-WEATHERAPP_IP = os.getenv('WEATHERAPP_IP', '127.0.0.1')
 
 def test_site_up():
     print("Running test_site_up...")
     try:
-        output = subprocess.check_output(f"curl http://{WEATHERAPP_IP}:9090 --max-time 3", shell=True)
+        output = subprocess.check_output("curl -s http://weatherapp_container:9090 --max-time 3", shell=True)
         print("Output:", output)
-        assert "Failed" not in str(output)
+        assert b"Failed" not in output
     except subprocess.CalledProcessError as e:
         print(f"Failed to connect: {e}")
         raise
@@ -17,9 +13,9 @@ def test_site_up():
 def test_connection():
     print("Running test_connection...")
     try:
-        output = subprocess.check_output(f"curl --include http://{WEATHERAPP_IP}:9090", shell=True)
+        output = subprocess.check_output("curl --include http://weatherapp_container:9090", shell=True)
         print("Output:", output)
-        status_code = int(str(output).split(" ")[1])
+        status_code = int(output.split()[1])
         print("Status Code:", status_code)
         assert status_code in range(200, 400)
     except subprocess.CalledProcessError as e:
@@ -27,6 +23,5 @@ def test_connection():
         raise
 
 if __name__ == '__main__':
-    time.sleep(5)  # Add a short sleep to ensure the service has time to start
     test_site_up()
     test_connection()
